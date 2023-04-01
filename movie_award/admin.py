bf44c59d4b84501f6c34fbd54f2eae3bad2c210a
iivy92 <pedro.ivo_@hotmail.com>
2023-04-01 18:26:31 -0300

    adding models and table

--- movie_award/admin.py
+++ movie_award/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from movie_award.models import Movie, Studio, Producer
 
-# Register your models here.
+class Movies(admin.ModelAdmin):
+    list_display = ('id', 'title', 'year')
+    list_display_links = ('id', 'title', 'year')
+    search_fields = ('id', 'title', 'year')
+
+admin.site.register(Movie, Movies)
+
+class Studios(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+admin.site.register(Studio, Studios)
+
+class Producers(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+admin.site.register(Producer, Producers)
--- movie_award/models.py
+++ movie_award/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
+class Studio(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
 
+class Producer(models.Model):
+    name = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.name
 
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    year = models.IntegerField()
+    studios = models.ManyToManyField(Studio)
+    producers = models.ManyToManyField(Producer)
 
+    def __str__(self):
+        return self.title
 
